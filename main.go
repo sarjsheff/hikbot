e243215ed21ec32be3558b2512349680dbc94fb3
Sergey Shevtsov <sarj.sheff@gmail.com>
2021-07-26 20:01:05 +0300

    Hikvision functions for working with the camera are moved to a separate repository

--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include "hiklib.h"
-// #include <string.h>
-//
-// extern int onmessage(int command, char *sDVRIP, char *pBuf, unsigned int dwBufLen);
-// extern void onmessagev30(LONG lCommand, NET_DVR_ALARMER *pAlarmer, char *pAlarmInfo, DWORD dwBufLen, void* pUserData);
-//
-// static NET_DVR_ALARMINFO_V30 getalarminfo(char *pAlarmInfo) {
-//    NET_DVR_ALARMINFO_V30 struAlarmInfo;
-//    memcpy(&struAlarmInfo, pAlarmInfo, sizeof(NET_DVR_ALARMINFO_V30));
-//    return struAlarmInfo;
-// }
-//
-// static void OnAlarmV30(int user,int alarmport) {
-//   HListenAlarmV30(user,alarmport,onmessagev30);
-// }
-// static void OnAlarm(int user,int alarmport) {
-//   HListenAlarm(user,alarmport,onmessage);
-// }
-// static void myprint(char* s) {
-//   printf("%s\n", s);
-// }
-import "C"
 import (
 	"encoding/json"
 	"flag"
@@ -39,16 +15,12 @@ import (
 	"strconv"
 	"strings"
 	"time"
-	"unsafe"
 
 	"github.com/google/uuid"
+	"github.com/sarjsheff/hiklib"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
-
-const COMM_ALARM int = 0x1100              //8000 Upload alarm message
-const COMM_ALARM_V30 int = 0x4000          //9000 upload alarm message
-const COMM_DEV_STATUS_CHANGED int = 0x7000 //Device status change alarm upload
 
 var ipParam = flag.String("c", "", "Camera IP address.")
 var userParam = flag.String("u", "", "Username.")
@@ -63,11 +35,11 @@ var zParam = flag.Int("z", 2, "Video preview rescale (divide).")
 
 var x1Param = flag.Bool("x1", false, "Issue 1.")
 
-type AlarmItem struct {
-	IP        string
-	Command   int
-	AlarmType int
-}
+// type AlarmItem struct {
+// 	IP        string
+// 	Command   int
+// 	AlarmType int
+// }
 
 type FFProbe struct {
 	Streams []map[string]interface{} `json:"streams"`
@@ -76,9 +48,12 @@ type FFProbe struct {
 
 var appid uuid.UUID = uuid.Must(uuid.NewRandom())
 
-var motions chan AlarmItem
-var dev = C.DevInfo{byStartChan: 0}
-var user = C.int(-1)
+var motions chan hiklib.AlarmItem
+
+// var dev = C.DevInfo{byStartChan: 0}
+// var user = C.int(-1)
+var dev = hiklib.DevInfo{ByStartChan: 0}
+var user = -1
 
 type touser string
 type MotionArea struct{ x, y, w, h float32 }
@@ -87,42 +62,41 @@ func (t touser) Recipient() string {
 	return string(t)
 }
 
-//export onmessagev30
-func onmessagev30(command C.int, pAlarmer *C.NET_DVR_ALARMER, pAlarmInfo *C.char, dwBufLen C.uint, pUserData unsafe.Pointer) {
-	i := AlarmItem{IP: C.GoString(&pAlarmer.sDeviceIP[0]), Command: int(command)}
-	switch int(command) {
-	case COMM_ALARM_V30:
-		log.Println("ALARM")
-		i.AlarmType = int(C.getalarminfo(pAlarmInfo).dwAlarmType)
-		motions <- i
-		break
-	case COMM_DEV_STATUS_CHANGED:
-		log.Printf("COMM_DEV_STATUS_CHANGED")
-		break
-	default:
-		log.Printf("Unknown Alarm [0x%x] !!!", command)
-	}
+// //export onmessagev30
+// func onmessagev30(command C.int, pAlarmer *C.NET_DVR_ALARMER, pAlarmInfo *C.char, dwBufLen C.uint, pUserData unsafe.Pointer) {
+// 	i := AlarmItem{IP: C.GoString(&pAlarmer.sDeviceIP[0]), Command: int(command)}
+// 	switch int(command) {
+// 	case COMM_ALARM_V30:
+// 		log.Println("ALARM")
+// 		i.AlarmType = int(C.getalarminfo(pAlarmInfo).dwAlarmType)
+// 		motions <- i
+// 		break
+// 	case COMM_DEV_STATUS_CHANGED:
+// 		log.Printf("COMM_DEV_STATUS_CHANGED")
+// 		break
+// 	default:
+// 		log.Printf("Unknown Alarm [0x%x] !!!", command)
+// 	}
+// }
 
-}
+// //export onmessage
+// func onmessage(command C.int, ip *C.char, data *C.char, ln C.uint) C.int {
 
-//export onmessage
-func onmessage(command C.int, ip *C.char, data *C.char, ln C.uint) C.int {
+// 	i := AlarmItem{IP: C.GoString(ip), Command: int(command)}
 
-	i := AlarmItem{IP: C.GoString(ip), Command: int(command)}
-
-	switch int(command) {
-	case COMM_ALARM_V30:
-		i.AlarmType = int(C.getalarminfo(data).dwAlarmType)
-		motions <- i
-		break
-	case COMM_DEV_STATUS_CHANGED:
-		log.Printf("COMM_DEV_STATUS_CHANGED %s %s", C.GoString(ip), C.GoString(data))
-		break
-	default:
-		log.Printf("Unknown Alarm [0x%x] %s %s !!!", command, C.GoString(ip), C.GoString(data))
-	}
-	return 1
-}
+// 	switch int(command) {
+// 	case COMM_ALARM_V30:
+// 		i.AlarmType = int(C.getalarminfo(data).dwAlarmType)
+// 		motions <- i
+// 		break
+// 	case COMM_DEV_STATUS_CHANGED:
+// 		log.Printf("COMM_DEV_STATUS_CHANGED %s %s", C.GoString(ip), C.GoString(data))
+// 		break
+// 	default:
+// 		log.Printf("Unknown Alarm [0x%x] %s %s !!!", command, C.GoString(ip), C.GoString(data))
+// 	}
+// 	return 1
+// }
 
 func bot() {
 
@@ -145,24 +119,25 @@ func bot() {
 	// var menu = &tb.ReplyMarkup{ResizeReplyKeyboard: true}
 	// var btnSettings = menu.Data("⚙", "Settings")
 	video := func(offset int, limit int) {
-		var v = C.MotionVideos{}
+		//var v = hiklib.MotionVideos{} //C.MotionVideos{}
 		mm, _ := b.Send(admin, "Fetch video from camera...")
-		C.HListVideo(user, &v)
-		b.Edit(mm, strconv.Itoa(int(v.count))+" video on camera.")
-		if v.count > 0 {
+		//C.HListVideo(C.int(user), &v)
+		_, v := hiklib.HikListVideo(user)
+		b.Edit(mm, strconv.Itoa(v.Count)+" video on camera.")
+		if v.Count > 0 {
 			txt := ""
 			if offset == 0 {
 				txt = fmt.Sprintf("First %d video:\n", limit)
 			} else {
 				txt = fmt.Sprintf("%d video from %d :\n", limit, offset)
 			}
-			for i := offset - 1; i < int(v.count) && i < offset+limit-1; i++ {
-				dt := time.Date(int(v.videos[i].from_year), time.Month(int(v.videos[i].from_month)), int(v.videos[i].from_day), int(v.videos[i].from_hour), int(v.videos[i].from_min), int(v.videos[i].from_sec), 0, time.UTC)
-				todt := time.Date(int(v.videos[i].to_year), time.Month(int(v.videos[i].to_month)), int(v.videos[i].to_day), int(v.videos[i].to_hour), int(v.videos[i].to_min), int(v.videos[i].to_sec), 0, time.UTC)
-				txt = txt + "<b>" + dt.Format("2006-01-02 15:04:05") + " - " + todt.Format("15:04:05") + "</b> /dl_" + C.GoString(v.videos[i].filename) + " \n"
-				videolist[C.GoString(v.videos[i].filename)] = dt.Format("2006-01-02/15:04:05")
+			for i := offset - 1; i < v.Count && i < offset+limit-1; i++ {
+				dt := time.Date(v.Videos[i].From_year, time.Month(v.Videos[i].From_month), v.Videos[i].From_day, v.Videos[i].From_hour, v.Videos[i].From_min, v.Videos[i].From_sec, 0, time.UTC)
+				todt := time.Date(v.Videos[i].To_year, time.Month(v.Videos[i].To_month), v.Videos[i].To_day, v.Videos[i].To_hour, v.Videos[i].To_min, v.Videos[i].To_sec, 0, time.UTC)
+				txt = txt + "<b>" + dt.Format("2006-01-02 15:04:05") + " - " + todt.Format("15:04:05") + "</b> /dl_" + v.Videos[i].Filename + " \n"
+				videolist[v.Videos[i].Filename] = dt.Format("2006-01-02/15:04:05")
 			}
-			if offset+limit < int(v.count) {
+			if offset+limit < v.Count {
 				txt = txt + fmt.Sprintf("<b>Next 10 video /video_%d_%d</b>\n", offset+limit, limit)
 			}
 			// menu.Inline(menu.Row(btnSettings))
@@ -177,13 +152,13 @@ func bot() {
 
 	snapshot := func(mareas bool) {
 		fname := filepath.Join(*datadirParam, fmt.Sprintf("%s.jpeg", uuid.Must(uuid.NewRandom()).String()))
-		err := C.HCaptureImage(user, dev.byStartChan, C.CString(fname))
+		err := hiklib.HikCaptureImage(user, dev.ByStartChan, fname)
 		if err > -1 {
 			caption := ""
 			if mareas {
-				var ma = C.MotionAreas{}
-				C.HMotionArea(user, &ma)
-
+				// var ma = C.MotionAreas{}
+				// C.HMotionArea(C.int(user), &ma)
+				_, ma := hiklib.HikMotionArea(user)
 				col := color.RGBA{255, 0, 0, 128}
 				var dst *image.RGBA
 				var b image.Rectangle
@@ -200,8 +175,8 @@ func bot() {
 				}
 				caption = caption + fmt.Sprintf("Image size %vx%v\n", b.Dx(), b.Dy())
 				for i := 0; i < 8; i++ {
-					if ma.areas[i].w > 0 && ma.areas[i].h > 0 {
-						x, y, w, h := int(float32(b.Dx())*float32(ma.areas[i].x)), int(float32(b.Dy())*float32(ma.areas[i].y)), int(float32(b.Dx())*float32(ma.areas[i].w)), int(float32(b.Dy())*float32(ma.areas[i].h))
+					if ma.Areas[i].W > 0 && ma.Areas[i].H > 0 {
+						x, y, w, h := int(float32(b.Dx())*float32(ma.Areas[i].X)), int(float32(b.Dy())*float32(ma.Areas[i].Y)), int(float32(b.Dx())*float32(ma.Areas[i].W)), int(float32(b.Dy())*float32(ma.Areas[i].H))
 						log.Printf("Area %v x:%v y:%v [%vx%v]\n", i+1, x, y, w, h)
 						caption = caption + fmt.Sprintf("Area %v x:%v y:%v [%vx%v]\n", i+1, x, y, w, h)
 						if dst != nil {
@@ -266,8 +241,8 @@ func bot() {
 	b.Handle("/reboot", func(m *tb.Message) {
 		<-done
 		if m.Sender.ID == *adminParam {
-			res := C.HReboot(user)
-			if int(res) > 0 {
+			res := hiklib.HikReboot(user)
+			if res > 0 {
 				b.Send(m.Sender, "Rebooting! Wait 10 sec.")
 				time.Sleep(10 * time.Second)
 				for Login() < 1 {
@@ -303,7 +278,9 @@ func bot() {
 							"acodec":   "none",
 							"movflags": "+faststart",
 						}
-						C.HSaveFile(user, C.CString(m.Text[4:]), C.CString(fname))
+
+						// C.HSaveFile(C.int(user), C.CString(m.Text[4:]), C.CString(fname))
+						hiklib.HikSaveFile(user, m.Text[4:], fname)
 						b.Edit(mm, "Probing...")
 						f, err := ffmpeg.Probe(fname)
 						var fjson FFProbe
@@ -397,12 +374,17 @@ func bot() {
 }
 
 func Login() int {
-	user = C.HLogin(C.CString(*ipParam), C.CString(*userParam), C.CString(*passParam), &dev)
+	// user = C.HLogin(C.CString(*ipParam), C.CString(*userParam), C.CString(*passParam), &dev)
+	user, dev = hiklib.HikLogin(*ipParam, *userParam, *passParam)
 	if int(user) > -1 {
 		if *x1Param {
-			C.OnAlarmV30(user, C.int(*alarmParam))
+			hiklib.HikOnAlarmV30(user, *alarmParam, func(item hiklib.AlarmItem) {
+				motions <- item
+			})
 		} else {
-			C.OnAlarm(user, C.int(*alarmParam))
+			hiklib.HikOnAlarm(user, *alarmParam, func(item hiklib.AlarmItem) {
+				motions <- item
+			})
 		}
 		return int(user)
 	} else {
@@ -416,19 +398,15 @@ func main() {
 	if *ipParam == "" || *userParam == "" || *passParam == "" || *adminParam == 0 || *tkeyParam == "" {
 		flag.PrintDefaults()
 	} else {
-		motions = make(chan AlarmItem, 100)
+		motions = make(chan hiklib.AlarmItem, 100)
 
-		cs := C.CString("")
-
-		C.HVersion(cs)
-		log.Printf("%s\n", C.GoString(cs))
+		log.Printf("%s\n", hiklib.HikVersion())
 		if Login() > -1 {
-			defer C.HLogout(user)
+			defer hiklib.HikLogout(user)
 
 			bot()
 		} else {
 			log.Println("Error login.")
 		}
-		C.free(unsafe.Pointer(cs))
 	}
 }
